606ac8d4bf106cd40fcfd552cb8fdf286fb68392
Shelmith <shelmithkimemia8@gmail.com>
2024-02-14 23:35:46 +0300

    added vid

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+
 import './about.css'
 import ME from '../../assets/pp.png'
-import {BiAward} from 'react-icons/bi'
-import {RiFoldersLine} from 'react-icons/ri'
-import {AiOutlineUsergroupAdd} from'react-icons/ai'
+import vid from "../../assets/aboutmevideo.mp4"
+
+
+import { BiAward } from 'react-icons/bi'
+import { RiFoldersLine } from 'react-icons/ri'
+import { AiOutlineUsergroupAdd } from 'react-icons/ai'
+
+
 const About = () => {
   return (
     <section id='about'>
@@ -11,37 +17,45 @@ const About = () => {
       <h2>About Me</h2>
       <div className="container about_container">
 
-      <div className="about_me">
-<div className="about_me-image">
-  <img src={ME} alt="" />
-</div>
-      </div>
-      <div className="about_content">
-<div className="about_cards">
-  <article className='about_card'>
-    <BiAward className='about_icon'/>
-    <h5>Experience</h5>
-    <small>2+ years freelancing Experience</small>
-  </article>
-  <article className='about_card'>
-    <AiOutlineUsergroupAdd className='about_icon'/>
-    <h5>CLients</h5>
-    <small>20+ worldwide</small>
-  </article>
-  <article className='about_card'>
-    <RiFoldersLine className='about_icon'/>
-    <h5>Projects</h5>
-    <small>Completed projects 
-      20+ projects</small>
-  </article>
-</div>
-<p>I'm Shelmith Wambui Kimemia, 21 years old student at Dedan Kimathi University Of Technology pursuing bachelor
-  of Science in Computer Science, In my final Year of studies.</p>
-  <a href="#contact" className='btn btn-primary'>Let's Talk</a>
-      </div>
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={ME} alt="" />
+          </div>
+        </div>
+        <div className="about_content">
+          <div className="about_cards">
+            <article className='about_card'>
+              <BiAward className='about_icon' />
+              <h5>Experience</h5>
+              <small>2+ years freelancing Experience</small>
+            </article>
+            <article className='about_card'>
+              <AiOutlineUsergroupAdd className='about_icon' />
+              <h5>CLients</h5>
+              <small>20+ worldwide</small>
+            </article>
+            <article className='about_card'>
+              <RiFoldersLine className='about_icon' />
+              <h5>Projects</h5>
+              <small>Completed projects
+                20+ projects</small>
+            </article>
+          </div>
+          <p>I'm Shelmith Wambui Kimemia, 21 years old student at Dedan Kimathi University Of Technology pursuing bachelor
+            of Science in Computer Science, In my final Year of studies.</p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+          <article className='portfolio_item'>
+            <video width="640" height="300" controls>
+              <source src={vid} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+            <h3>Video About Me</h3>
+            <div className="portfolio_item-cta">
+            </div>
+          </article>    </div>
       </div>
     </section>
   )
-} 
+}
 
 export default About
